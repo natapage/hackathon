0dc87570ab597b097673947ca88b9aa503a7a737
natapage <624832@gmail.com>
2024-01-15 11:30:58 +0600

    project created

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref();
+</script>
+
+<template>
+  <header class="h-[131px] w-full">
+    <div>js Quiz</div>
+    <div>Count: {{ count }}/10</div>
+  </header>
+  <div class="h-screen w-full bg-gray-200"></div>
+</template>
+
+<style scoped></style>
